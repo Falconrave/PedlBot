aec05494e980f1b6cbceef3ca48a03b3a2b0c7fb
Falconrave <penguintronder@gmail.com>
2017-08-11 16:28:27 -0700

    Edit main file

--- pedl-bot.py
+++ pedl-bot.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python!"
+    return "Hello world!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
